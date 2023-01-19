7664a37c5bccfb2acad8d54527b832ad8c1f2d2c
TheDarkGuardian <56185602+TheDarkGuardian@users.noreply.github.com>
2023-01-19 13:10:47 +0300

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ function Navigation() {
   return (
     <footer className="p-4 bg-white md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 flex-none">
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        © 2022{' '}
+        © 2023{' '}
         <a href="/#" className="hover:underline">
           Arda Baran Akdemir
         </a>
